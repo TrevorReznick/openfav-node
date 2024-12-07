d9e28bf2729b4365ac15101d540a684280e851d2
Trevor Reznick <electrics.sheeps@gmail.com>
2024-12-07 12:22:51 +0100

    OPF-65 deleted db query mvc, leave essential

--- app/controllers/index.ts
+++ app/controllers/index.ts
@@ -2,10 +2,8 @@ import { Model } from '../models/index'
 //import Response from 'express';
 import {__email as msg, do_mail as mailer} from '../scripts/mailer'
 import { MailMessage } from '../interfaces/MailerModel'
-import { generateImage, getImage } from '../scripts/imageGenerator'
 import { doChat } from '../scripts/chatGpt'
-import { MessageType, PromptMsg} from '../types/types'
-import { RequestById,  RequestMail, RequestPrompt, Response} from '../interfaces/ResponsesRequests'
+import { RequestById,  RequestMail, Response} from '../interfaces/ResponsesRequests'
 
 
 export class MainController {
@@ -18,29 +16,7 @@ export class MainController {
             res.status(500).send(err)
         }
     }
-    static async getAllUsers(req: RequestById, res: Response) {
-        try {
-            const users = await Model.queryAll()
-            res.send(users)
-        } catch (err) {
-            res.status(500).send(err)
-        }
-    }
-    static async getById(req: RequestById, res: Response) {
-        const id = req.params.id
-        if(!req.params.id){
-            return res.status(400).send('Id parameter is required')
-        } else {
-            const user = await Model.queryById(id)
-            .catch(err => {
-                res.status(500).send(err)
-            })
-            if(!user){
-                return res.status(404).send('User not found')
-            }
-            else res.send(user)
-        }
-    }
+    
     static sendMail(req: RequestMail, res: Response) {
 
         console.log('Request Body:', req.body)
@@ -78,45 +54,7 @@ export class MainController {
         } catch(err) {
             res.status(500).send(err)
         }
-    }
-    /* @@ huggingface transformers @@ */
-
-    static doImage(req: RequestPrompt, res: Response) {
-
-        try {
-            generateImage(`
-                a cat dressed as witch for halloweeen
-            `)
-            const message = 'Image was created successully! Check in your public path'
-            res.send(message)
-        } catch (e) {
-            console.log(e)
-        }
-
-    }
-
-    
-
-    static getImage(req: RequestPrompt, res: Response) {
-        try {
-            //generateImage("photo of a great boat in a river")
-            console.log('call img buffer')
-            const image64base = getImage('photo of a great boat in a river')
-            //const message = 'Image was created successully! Check in your public path'
-            //console.log('send img 64base')
-            //console.log(image64base)
-            res.send(image64base)
-        } catch (e) {
-            console.log(e)
-        }
-
-    }
-
-    /* @@ chat GPT @@ */
-
-    static async testPost(req: any, res: Response) {
-        res.send('hello from post!')        
-    }
+    }   
 
 
     static async doGptChat(req: any, res: Response) {
@@ -133,37 +71,4 @@ export class MainController {
     }
 
 }
-
-
-/*async getById(req: Request, res: Response) {
-    try {
-      const id = parseInt(req.params.id);  
-      const user = await UserModel.getById(id);
-      res.send(user);
-    } catch (err) {
-      res.status(500).send(err);
-    }
- }
-*/
-
-
-
-/*
-export const main = {
-
-    test: async (req: any, res: any) => {
-
-        try {  
-            const nome = req.query.nome
-            const cognome = req.query.cognome
-            const email = req.query.email
-            const data = await Model.test()    
-            res.send(data)    
-        } catch (err) {
-            res.status(500).send(err);
-        }
-
-    }
-}
-*/
 
--- app/routes/index.ts
+++ app/routes/index.ts
@@ -5,32 +5,15 @@ import { MainController as main} from '../controllers'
 const router = express.Router()
 
 router.get('/', main.home)
-router.get('/test-db', main.getAllUsers)
 router.get('/test-api', main.testApi)
-router.get('/find-user/:id', main.getById)
 router.get('/test-mail', main.testMailer)
 router.post('/send-email', main.sendMail as any)
 /* @@ new routes @@ */
-router.post('/generate', main.doImage)
-router.post('/get-img', main.getImage)
-router.post('/chat', main.doGptChat)
 
-
-/* test route params 
-router.post('/send-email', (req, res) => {
-  console.log('Request Body:', req.body);
-
-  // Resto del codice del tuo gestore di route
-})
-*/
 
 export default router
 
-/*
-export function test(req: Request, res: Response) {
-  res.send('Testing Routes')
-}
-*/
+
 
 
 
